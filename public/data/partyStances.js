b3c15e42e587f7c8d2778ad0e57334dcc3cb18bf
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-04-25 13:19:04 +0100

    partyStances file

---
+++ public/data/partyStances.js
@@ -0,0 +1,51 @@
+var partyStances = [
+	opinions: {
+      issues: {
+        "brexit": {
+          debates: {
+            "brexit-level": {
+              parties: {
+                "conservative": {
+                  opinion: 1
+                }, "labour": {
+                  opinion: 0.8
+                }, "Lib Dem": {
+                  opinion: 0.6
+                }, "UKIP": {
+                  opinion: 1
+                }, "SNP": {
+                  opinion: 0.2
+                }, "Green": {
+                  opinion: 0.6
+                }, "Plaid Cymru": {
+                  opinion: 0.6
+                }
+              }
+            },
+            "mp-vote": {
+              parties: {
+                "conservative": {
+                  opinion: 0
+                }, "labour": {
+                  opinion: 0.5
+                }, "conservative": {
+                  opinion: 1
+                }, "labour": {
+                  opinion: 0
+                }, "conservative": {
+                  opinion: 1
+                }, "labour": {
+                  opinion: 1
+                }, "labour": {
+                  opinion: 0.5
+                }
+              }
+            }
+          }
+        },
+        "health": {
+
+        }
+      }
+    }
+]
