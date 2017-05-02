afafc3303376682a7d4ced3cf140baa3233d684b
Igor Pavlov <igor@pavlov>
2017-05-02 14:43:26 +0100

    Fixing merge problems

--- public/bundle.js
+++ public/bundle.js
@@ -5150,12 +5150,14 @@ function createObjectProps(globalObject, props) {
 module.exports = new APIService();
 
 },{"httpism":6}],67:[function(require,module,exports){
-var hyperdom = require('hyperdom'),
-    h = hyperdom.html,
-    router = require('hyperdom-router'),
-    windowEvents = require('hyperdom/windowEvents'),
-    api = require('../services/APIService'),
-    http = require('httpism');
+const
+  hyperdom = require('hyperdom'),
+  h = hyperdom.html,
+  router = require('hyperdom-router'),
+  windowEvents = require('hyperdom/windowEvents'),
+  api = require('../services/APIService'),
+  http = require('httpism')
+;
 
 var routes = {
   root: router.route('/'),
@@ -5184,13 +5186,6 @@ var WideDecide = function() {
 class App {
   constructor(data) {
     this.header = new Header();
-
-    var templateUrl = '//localhost:5002/templates';
-    http.get(templateUrl)
-    .then(function (res) { //Must make sure this isn't needed before it returns, since it's asynchronous!
-      CardTemplates = res.body;
-      console.log(CardTemplates)
-    });
 
     var issueKeys = Object.keys(partyStances.opinions.issues);
     issueKeys.forEach(function(issueKey, i) {
@@ -5365,12 +5360,6 @@ class Dashboard {
       h("h2", this.dashboard.subtitle),
       h("section.tasks", tasksDOM)
     )
-  }
-}
-
-class Question {
-  constructor(params) {
-
   }
 }
 
@@ -5735,7 +5724,63 @@ class CardContent {
         break;
 
       case 'postcode-compare':
-        var data = self.data;
+        var data = this.data;
+        // temp: this is for testing loops with real constituencyResults data
+        /*
+        this.data.constituencyResults = {
+          heading: "Header",
+          subheading: "Subheader",
+          constituencies: [
+            {
+              type: "constituency",
+              location: "London",
+              parties: "One vs Two"
+            },
+            {
+              type: "constituency",
+              location: "Yorkshire",
+              parties: "Three vs Four"
+            }
+          ]
+        }
+        */
+        data.postcodeSubmit = function(e){
+          e.stopPropagation();
+          model.user.isWaiting = true;
+          api.comparePostcodes(model.user.postcode, model.user.postcode_uni).then(function(results){
+            if (results.error) {
+              console.log("Sorry, we didn't recognise that postcode!")
+              routes.step({
+                name: 'postcode-compare',
+                type: 'step',
+                error: 'bad-postcode',
+              }).replace();
+            } else {
+              model.user.isWaiting = false;
+              model.user.resultsCompare.push(results);
+              routes.step({
+                name: 'postcode-compare',
+                type: 'step',
+                next: data.nextStep,
+                attempt: model.user.resultsCompare.length
+              }).replace();
+            }
+          });
+          return false;
+        }
+        if(model.user.resultsCompare.length){
+          const latestResults = model.user.resultsCompare[model.user.resultsCompare.length-1];
+          data.constituencyResults = {
+            heading: latestResults.text.heading,
+            subheading: latestResults.text.subheading,
+            constituencies: latestResults.seats // todo: fix "type" here
+          }
+        }
+        console.log("WOWOWOW")
+        console.log(data.constituencyResults)
+        data.postcodeBinding = [model.user, 'postcode'];
+        data.postcodeUniBinding = [model.user, 'postcode_uni'];
+        return h('div', getCardDom(data, CardTemplates['postcodeCompare']));
         return h('.content',
           h('h2', { 'class': {'hide': model.user.resultsCompare.length }}, self.data.name),
           h('div.body-content',
@@ -6183,14 +6228,25 @@ var markdownToHtml = function(text) {
 
 var getCardDom = function(data, template) {
   data.type = data.type || (data["@type"] ? data["@type"].split('/')[data["@type"].split('/').length-1] : 'Detail');
-  var dom = [];
-  template.forEach(function(element) {
+  const dom = template.map(function(element) {
     var content,
         attr = {};
-    if (element.template)
+    if(
+      element.condition
+      &&
+      (
+        !getObjectPathProperty(data, element.condition) && !element.condition.match(/^!/)
+        ||
+        getObjectPathProperty(data, element.condition.replace(/^!/),"") && element.condition.match(/^!/)
+      )
+    )
+      return undefined;
+    else if (element.template)
       content = getCardDom(data, CardTemplates[element.template.var ? getObjectPathProperty(data, element.template.var) : element.template])
     else if (!element.content)
       content = '';
+    else if (element.loop)
+      content = getObjectPathProperty(data, element.loop).map(function(el){return getCardDom(el, element.content)});
     else if (element.content.constructor === Array)
       content = getCardDom(data, element.content);
     else if (element.content.var)
@@ -6218,14 +6274,328 @@ var getCardDom = function(data, template) {
       })
     }
     if (element.content && element.content.markdown) {
-      dom.push(h.rawHtml(element.dom, attr, markdownToHtml(content)));
+      return h.rawHtml(element.dom, attr, markdownToHtml(content));
     } else {
-      dom.push(h(element.dom, attr, content));
+      return h(element.dom, attr, content);
     }
   });
   return dom;
 }
 
-hyperdom.append(document.body, new App());
+const loadTemplates = function(templateUrl){
+  return new Promise(function(resolve,reject){
+    http.get(templateUrl)
+    .then(function (res) {
+      resolve(res.body);
+    });
+  });
+}
+
+const _temporaryTemplates = function(){
+  CardTemplates.card = [
+    {
+      "dom": "div.card",
+      "attr": {
+        "data-uri": {
+          "var": "@id",
+        },
+        "style": "height: auto"
+      },
+      "content": [
+        {
+          "dom": "div.card-visible",
+          "content": [
+            {
+              "dom": "div.close",
+              "content": [
+                {
+                  "dom": "i.fa.fa-times",
+                  "attr": {
+                    "data-hidden": "true"
+                  }
+                }
+              ]
+            },
+            {
+              "dom": "div.content",
+              "attr": {
+                "class": {
+                  "var": "@type"
+                }
+              },
+              "template": {
+                "var": "type"
+              }
+            },
+            {
+              "dom": "a.card-icon",
+              "attr": {
+                "target": "_blank",
+                "tabindex": "-1"
+              },
+              "content": [
+                {
+                  "dom": "img",
+                  "attr": {
+                    "src": "http://app.explaain.com/card-logo.png"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "dom": "button.edit-button",
+          "attr": {
+            "tabindex": "-1"
+          },
+          "content": [
+            {
+              "dom": "i.fa.fa-pencil",
+              "attr": {
+                "aria-hidden": "true"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  CardTemplates["Person"] = [
+    {
+      "dom": "h2",
+      "content": {
+        "var": "name"
+      }
+    },
+    {
+      "dom": "div.card-image",
+      "content": [{
+        "dom": "img",
+        "attr": {"src": {
+          "var": "image"
+        }}
+      }]
+    },
+    {
+      "dom": "div.body-content",
+      "content": {
+        "var": "description",
+        "markdown": true
+      }
+    }
+  ];
+  CardTemplates.postcodeCompare = [
+    {
+      "dom": "h2",
+      "content": {
+        "var": "name",
+        "default": "Please enter your postcode"
+      }
+    },
+    {
+      "dom": "div.body-content",
+      "content": [
+        {
+          "dom": "form.postcode-form",
+          "attr": {
+            "onsubmit": {
+              "var": "postcodeSubmit"
+            }
+          },
+          "content": [
+            {
+              "dom": "input.form-control",
+              "attr": {
+                "autofocus": "true",
+                "type": "text",
+                "name": "postcode",
+                "placeholder": "Home Postcode",
+                "binding": {
+                  "var": "postcodeBinding"
+                }
+              }
+            },
+            {
+              "dom": "input.form-control",
+              "attr": {
+                "autofocus": "true",
+                "type": "text",
+                "name": "postcode",
+                "placeholder": "Uni Postcode",
+                "binding": {
+                  "var": "postcodeUniBinding"
+                }
+              }
+            },
+            {
+              "dom": "button.btn.btn-success",
+              "attr": {
+                "type": "submit"
+              },
+              "content": "Compare"
+            }
+          ]
+        },
+        {
+          "dom": "h3",
+          "content": {
+            "var": "subheading"
+          }
+        },
+        {
+          "dom": "p",
+          "content": {
+            "var": "description",
+            "markdown": true
+          }
+        },
+        {
+          "dom": "div",
+          "condition": "constituencyResults",
+          "template": "constituencyResults"
+        }
+      ]
+    },
+    {
+      "dom": "div",
+      "template": "footer"
+    }
+  ]
+
+  CardTemplates.footer = [
+    {
+      "dom": ".footer",
+      "content": [
+        {
+          "dom": "div",
+          "template": {
+            "var": "footerContentTemplate"
+          }
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.voteNow = [
+    {
+      "dom": "div",
+      "content": [
+        {
+          "dom": ".bold",
+          "content": "or go straight to register"
+        },
+        {
+          "dom": "p",
+          "content": [
+            {
+              "dom": "a.discard-card-style",
+              "attr": {
+                "href": "https://www.gov.uk/register-to-vote",
+                "target":"_blank",
+              },
+              "content": [
+                {
+                  "dom": "button.btn.btn-primary",
+                  "content": "Register >"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "dom": "p.small",
+          "content": "This link will take you to the official gov.uk website"
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.constituencyResults = [
+    {
+      "dom": ".seats",
+      "content": [
+        {
+          "dom": "div",
+          "content": {
+            "var": "constituencyResults.heading"
+          }
+        },
+        {
+          "dom": "div",
+          "content": {
+            "var": "constituencyResults.subheading"
+          }
+        },
+        {
+          "dom": ".seat-list",
+          "loop": "constituencyResults.constituencies",
+          "content": [
+            {
+              "dom": "span",
+              "template": "constituency"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.constituency = [
+    {
+      "dom": "div.seat.column50",
+      "content": [
+        {
+          "dom": "div.location.small",
+          "content": {
+            "var": "location"
+          },
+        },
+        {
+          "dom": "div.versus.bold.line1em",
+          "content": {
+            "var": "parties"
+          }
+        }
+      ]
+    }
+  ]
+
+  // Usage:
+  // return h('div', getCardDom({type: "people", people: [{type: "person", name: "Sarah", age: "26"},{type: "person", name: "Chris", age: "34"}]}, CardTemplates['loopExample']));
+  CardTemplates.loopExample = [
+    {
+      "dom": ".people",
+      "loop": "people", // changing the scope of data
+      "content": [
+        {
+          "dom": ".person",
+          "content": [
+            {
+              "dom": "div",
+              "content": {
+                "var": "name"
+              }
+            },
+            {
+              "dom": "div",
+              "content": {
+                "var": "age"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+
+const templatesUrl = '//explaain-api.herokuapp.com/templates';
+loadTemplates(templatesUrl).then(function(_templates){
+  CardTemplates = _templates;
+  _temporaryTemplates(); // todo: this is needed for development purposes, move templated to backend once tested
+  hyperdom.append(document.body, new App());
+});
 
 },{"../models/model":1,"../services/APIService":66,"httpism":6,"hyperdom":20,"hyperdom-router":14,"hyperdom/windowEvents":36}]},{},[67]);
--- views/index.js
+++ views/index.js
@@ -1,9 +1,11 @@
-var hyperdom = require('hyperdom'),
-    h = hyperdom.html,
-    router = require('hyperdom-router'),
-    windowEvents = require('hyperdom/windowEvents'),
-    api = require('../services/APIService'),
-    http = require('httpism');
+const
+  hyperdom = require('hyperdom'),
+  h = hyperdom.html,
+  router = require('hyperdom-router'),
+  windowEvents = require('hyperdom/windowEvents'),
+  api = require('../services/APIService'),
+  http = require('httpism')
+;
 
 var routes = {
   root: router.route('/'),
@@ -32,13 +34,6 @@ var WideDecide = function() {
 class App {
   constructor(data) {
     this.header = new Header();
-
-    var templateUrl = '//localhost:5002/templates';
-    http.get(templateUrl)
-    .then(function (res) { //Must make sure this isn't needed before it returns, since it's asynchronous!
-      CardTemplates = res.body;
-      console.log(CardTemplates)
-    });
 
     var issueKeys = Object.keys(partyStances.opinions.issues);
     issueKeys.forEach(function(issueKey, i) {
@@ -213,12 +208,6 @@ class Dashboard {
       h("h2", this.dashboard.subtitle),
       h("section.tasks", tasksDOM)
     )
-  }
-}
-
-class Question {
-  constructor(params) {
-
   }
 }
 
@@ -583,7 +572,63 @@ class CardContent {
         break;
 
       case 'postcode-compare':
-        var data = self.data;
+        var data = this.data;
+        // temp: this is for testing loops with real constituencyResults data
+        /*
+        this.data.constituencyResults = {
+          heading: "Header",
+          subheading: "Subheader",
+          constituencies: [
+            {
+              type: "constituency",
+              location: "London",
+              parties: "One vs Two"
+            },
+            {
+              type: "constituency",
+              location: "Yorkshire",
+              parties: "Three vs Four"
+            }
+          ]
+        }
+        */
+        data.postcodeSubmit = function(e){
+          e.stopPropagation();
+          model.user.isWaiting = true;
+          api.comparePostcodes(model.user.postcode, model.user.postcode_uni).then(function(results){
+            if (results.error) {
+              console.log("Sorry, we didn't recognise that postcode!")
+              routes.step({
+                name: 'postcode-compare',
+                type: 'step',
+                error: 'bad-postcode',
+              }).replace();
+            } else {
+              model.user.isWaiting = false;
+              model.user.resultsCompare.push(results);
+              routes.step({
+                name: 'postcode-compare',
+                type: 'step',
+                next: data.nextStep,
+                attempt: model.user.resultsCompare.length
+              }).replace();
+            }
+          });
+          return false;
+        }
+        if(model.user.resultsCompare.length){
+          const latestResults = model.user.resultsCompare[model.user.resultsCompare.length-1];
+          data.constituencyResults = {
+            heading: latestResults.text.heading,
+            subheading: latestResults.text.subheading,
+            constituencies: latestResults.seats // todo: fix "type" here
+          }
+        }
+        console.log("WOWOWOW")
+        console.log(data.constituencyResults)
+        data.postcodeBinding = [model.user, 'postcode'];
+        data.postcodeUniBinding = [model.user, 'postcode_uni'];
+        return h('div', getCardDom(data, CardTemplates['postcodeCompare']));
         return h('.content',
           h('h2', { 'class': {'hide': model.user.resultsCompare.length }}, self.data.name),
           h('div.body-content',
@@ -1031,14 +1076,25 @@ var markdownToHtml = function(text) {
 
 var getCardDom = function(data, template) {
   data.type = data.type || (data["@type"] ? data["@type"].split('/')[data["@type"].split('/').length-1] : 'Detail');
-  var dom = [];
-  template.forEach(function(element) {
+  const dom = template.map(function(element) {
     var content,
         attr = {};
-    if (element.template)
+    if(
+      element.condition
+      &&
+      (
+        !getObjectPathProperty(data, element.condition) && !element.condition.match(/^!/)
+        ||
+        getObjectPathProperty(data, element.condition.replace(/^!/),"") && element.condition.match(/^!/)
+      )
+    )
+      return undefined;
+    else if (element.template)
       content = getCardDom(data, CardTemplates[element.template.var ? getObjectPathProperty(data, element.template.var) : element.template])
     else if (!element.content)
       content = '';
+    else if (element.loop)
+      content = getObjectPathProperty(data, element.loop).map(function(el){return getCardDom(el, element.content)});
     else if (element.content.constructor === Array)
       content = getCardDom(data, element.content);
     else if (element.content.var)
@@ -1066,12 +1122,326 @@ var getCardDom = function(data, template) {
       })
     }
     if (element.content && element.content.markdown) {
-      dom.push(h.rawHtml(element.dom, attr, markdownToHtml(content)));
+      return h.rawHtml(element.dom, attr, markdownToHtml(content));
     } else {
-      dom.push(h(element.dom, attr, content));
+      return h(element.dom, attr, content);
     }
   });
   return dom;
 }
 
-hyperdom.append(document.body, new App());
+const loadTemplates = function(templateUrl){
+  return new Promise(function(resolve,reject){
+    http.get(templateUrl)
+    .then(function (res) {
+      resolve(res.body);
+    });
+  });
+}
+
+const _temporaryTemplates = function(){
+  CardTemplates.card = [
+    {
+      "dom": "div.card",
+      "attr": {
+        "data-uri": {
+          "var": "@id",
+        },
+        "style": "height: auto"
+      },
+      "content": [
+        {
+          "dom": "div.card-visible",
+          "content": [
+            {
+              "dom": "div.close",
+              "content": [
+                {
+                  "dom": "i.fa.fa-times",
+                  "attr": {
+                    "data-hidden": "true"
+                  }
+                }
+              ]
+            },
+            {
+              "dom": "div.content",
+              "attr": {
+                "class": {
+                  "var": "@type"
+                }
+              },
+              "template": {
+                "var": "type"
+              }
+            },
+            {
+              "dom": "a.card-icon",
+              "attr": {
+                "target": "_blank",
+                "tabindex": "-1"
+              },
+              "content": [
+                {
+                  "dom": "img",
+                  "attr": {
+                    "src": "http://app.explaain.com/card-logo.png"
+                  }
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "dom": "button.edit-button",
+          "attr": {
+            "tabindex": "-1"
+          },
+          "content": [
+            {
+              "dom": "i.fa.fa-pencil",
+              "attr": {
+                "aria-hidden": "true"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  CardTemplates["Person"] = [
+    {
+      "dom": "h2",
+      "content": {
+        "var": "name"
+      }
+    },
+    {
+      "dom": "div.card-image",
+      "content": [{
+        "dom": "img",
+        "attr": {"src": {
+          "var": "image"
+        }}
+      }]
+    },
+    {
+      "dom": "div.body-content",
+      "content": {
+        "var": "description",
+        "markdown": true
+      }
+    }
+  ];
+  CardTemplates.postcodeCompare = [
+    {
+      "dom": "h2",
+      "content": {
+        "var": "name",
+        "default": "Please enter your postcode"
+      }
+    },
+    {
+      "dom": "div.body-content",
+      "content": [
+        {
+          "dom": "form.postcode-form",
+          "attr": {
+            "onsubmit": {
+              "var": "postcodeSubmit"
+            }
+          },
+          "content": [
+            {
+              "dom": "input.form-control",
+              "attr": {
+                "autofocus": "true",
+                "type": "text",
+                "name": "postcode",
+                "placeholder": "Home Postcode",
+                "binding": {
+                  "var": "postcodeBinding"
+                }
+              }
+            },
+            {
+              "dom": "input.form-control",
+              "attr": {
+                "autofocus": "true",
+                "type": "text",
+                "name": "postcode",
+                "placeholder": "Uni Postcode",
+                "binding": {
+                  "var": "postcodeUniBinding"
+                }
+              }
+            },
+            {
+              "dom": "button.btn.btn-success",
+              "attr": {
+                "type": "submit"
+              },
+              "content": "Compare"
+            }
+          ]
+        },
+        {
+          "dom": "h3",
+          "content": {
+            "var": "subheading"
+          }
+        },
+        {
+          "dom": "p",
+          "content": {
+            "var": "description",
+            "markdown": true
+          }
+        },
+        {
+          "dom": "div",
+          "condition": "constituencyResults",
+          "template": "constituencyResults"
+        }
+      ]
+    },
+    {
+      "dom": "div",
+      "template": "footer"
+    }
+  ]
+
+  CardTemplates.footer = [
+    {
+      "dom": ".footer",
+      "content": [
+        {
+          "dom": "div",
+          "template": {
+            "var": "footerContentTemplate"
+          }
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.voteNow = [
+    {
+      "dom": "div",
+      "content": [
+        {
+          "dom": ".bold",
+          "content": "or go straight to register"
+        },
+        {
+          "dom": "p",
+          "content": [
+            {
+              "dom": "a.discard-card-style",
+              "attr": {
+                "href": "https://www.gov.uk/register-to-vote",
+                "target":"_blank",
+              },
+              "content": [
+                {
+                  "dom": "button.btn.btn-primary",
+                  "content": "Register >"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "dom": "p.small",
+          "content": "This link will take you to the official gov.uk website"
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.constituencyResults = [
+    {
+      "dom": ".seats",
+      "content": [
+        {
+          "dom": "div",
+          "content": {
+            "var": "constituencyResults.heading"
+          }
+        },
+        {
+          "dom": "div",
+          "content": {
+            "var": "constituencyResults.subheading"
+          }
+        },
+        {
+          "dom": ".seat-list",
+          "loop": "constituencyResults.constituencies",
+          "content": [
+            {
+              "dom": "span",
+              "template": "constituency"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+
+  CardTemplates.constituency = [
+    {
+      "dom": "div.seat.column50",
+      "content": [
+        {
+          "dom": "div.location.small",
+          "content": {
+            "var": "location"
+          },
+        },
+        {
+          "dom": "div.versus.bold.line1em",
+          "content": {
+            "var": "parties"
+          }
+        }
+      ]
+    }
+  ]
+
+  // Usage:
+  // return h('div', getCardDom({type: "people", people: [{type: "person", name: "Sarah", age: "26"},{type: "person", name: "Chris", age: "34"}]}, CardTemplates['loopExample']));
+  CardTemplates.loopExample = [
+    {
+      "dom": ".people",
+      "loop": "people", // changing the scope of data
+      "content": [
+        {
+          "dom": ".person",
+          "content": [
+            {
+              "dom": "div",
+              "content": {
+                "var": "name"
+              }
+            },
+            {
+              "dom": "div",
+              "content": {
+                "var": "age"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+
+const templatesUrl = '//explaain-api.herokuapp.com/templates';
+loadTemplates(templatesUrl).then(function(_templates){
+  CardTemplates = _templates;
+  _temporaryTemplates(); // todo: this is needed for development purposes, move templated to backend once tested
+  hyperdom.append(document.body, new App());
+});
