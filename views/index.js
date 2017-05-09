2350eb04b54463aa560f5f96d79eee0a4e0861f9
Igor Pavlov <igor@pavlov>
2017-05-09 19:09:14 +0100

    Fixed mixpanel

--- views/index.js
+++ views/index.js
@@ -509,10 +509,10 @@ class CardContent {
               }
             ];
             if (results.error) {
-              trackEvent("STU: Wrong Postcodes: " + model.user.postcode + " " + model.user.postcode_uni);
+              trackEvent("Wrong Postcodes: " + model.user.postcode + " " + model.user.postcode_uni);
               helpers.throwError("Sorry, we didn't recognise that postcode!")
             } else {
-              trackEvent("STU: Received Results");
+              trackEvent("Received Results");
               model.user.resultsCompare.push(results);
             }
             self.refresh();
