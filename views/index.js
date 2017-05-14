75e13777587d078f23ed1414b2a18ad813196fb3
Josh Balfour <josh@joshbalfour.co.uk>
2017-05-14 10:45:32 +0100

    styling tweaks

--- views/index.js
+++ views/index.js
@@ -85,9 +85,9 @@ class App {
             })
             var currentPhrase = new PhraseSelect({phrase: params.name, next: params.next});
             var goto = model.myPhrases[params.name].goto;
-            var goButton = goto ? routes[goto.type]({name: goto.name}).a({class: '.btn.btn-success'}, 'Let\'s go!') : '';
-            var quizButton = routes.dashboard({name: 'decide', task: 'decide'}).a({class: '.btn.btn-primary'}, 'Just take me to the Quiz');
-            return h("div", h("h1", "What do you want to do?"), hPhrases, currentPhrase, goButton, h('div', quizButton));
+            var goButton = goto ? routes[goto.type]({name: goto.name}).a({class: 'btn btn-success'}, 'Let\'s go!') : '';
+            var quizButton = routes.dashboard({name: 'decide', task: 'decide'}).a({class: 'btn btn-primary'}, 'Just take me to the Quiz');
+            return h("div", h("h1", "What do you want to do?"), hPhrases, currentPhrase, goButton, h('div', {class: 'quiz-btn-container'}, quizButton));
           }),
 
           routes.step(function (params) {
