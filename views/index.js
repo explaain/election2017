451585463a06be82cb36eb4c785eaece9bd95981
Igor Pavlov <igor@pavlov>
2017-05-18 15:00:23 +0300

    Added some mixpanel events

--- views/index.js
+++ views/index.js
@@ -1432,6 +1432,7 @@ class Quiz {
       if(model.user.quizProgress.opinions.length<quizQuestions.length){
         self.refresh();
       } else {
+        trackEvent("Results Got",{type: "Quiz"});
         model.user.quizProgress.quizRezults = true;
         self.refresh();
         //TODO: Jeremy, you might want to change this :)
@@ -1460,6 +1461,7 @@ class Quiz {
       self.next();
     }
     self.startQuiz = function(){
+      trackEvent("Quiz Started",{type: "Quiz"});
       model.user.quizProgress.quizStarted = true;
       self.next();
     }
@@ -1604,6 +1606,7 @@ class Quiz {
     }
     self.countriesData.forEach(function(country){
       country.select = function(){
+        trackEvent("Country Selected",{type: "Quiz", code: country.code});
         model.user.quizProgress.country = country; // we set the whole country object here
         self.next();
       }
