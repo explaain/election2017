59d6df22136dbafef159d172a23a51a20c47b78f
Jan Baykara <jan@baykara.co.uk>
2017-05-30 01:45:43 +0100

    Enforced share URL update on back/skip/next Q

--- views/index.js
+++ views/index.js
@@ -1853,15 +1853,14 @@ class Quiz {
     }
 
     self.next = function() {
-      self.updateShareLinks();
-
       if (qp.questionPointer > 0) qp.startingQuiz = false;
       trackEvent("Question Answered",{type: "Quiz", questionNumber: qp.answers.length, questionId: qp.questionSeries[qp.questionPointer], answer: qp.answers[qp.answers.length-1], opinion: qp.opinions[qp.questionPointer-1]});
       // console.log("Quiz",  qp.answers.length,  qp.questionSeries[qp.questionPointer],  quiz.questionDB[qp.questionSeries.length-1],  qp.answers[qp.answers.length-1],  qp.opinions[qp.questionPointer-1] );
       console.log(qp.answers.length);
       console.log(quiz.questionDB[qp.questionSeries.length-1]);
       if(qp.questionPointer < qp.questionSeries.length){
-        self.refresh();
+        // self.updateShareLinks();
+        // self.refresh();
       } else {
         trackEvent("Results Got",{type: "Quiz", party: qp.resultsData[0].name, percentage: qp.resultsData[0].percentage, isDraw: (qp.resultsData.length>1)});
         qp.quizResults = true;
@@ -1869,8 +1868,9 @@ class Quiz {
         qp.country.parties.forEach(function(_party){
           _party.quizResults = true;
         });
-        self.refresh();
       }
+      self.updateShareLinks();
+      self.refresh();
     }
 
     /* ---
@@ -1931,7 +1931,9 @@ class Quiz {
         //   }
         // }
 
+        self.updateShareLinks();
         self.defineCalculableTopics();
+        self.refresh();
         self.next();
         return false;
       }
@@ -1981,6 +1983,8 @@ class Quiz {
       console.log(newScores);
       self.defineCalculableTopics();
       self.updatePartyPercentages(newScores);
+      self.updateShareLinks();
+      self.refresh();
     }
 
     self.recalculateOpinions = function() {
@@ -2061,6 +2065,7 @@ class Quiz {
         }
       }
 
+      self.updateShareLinks();
       self.refresh();
       self.next();
     }
