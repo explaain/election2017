1f2ae7d8da4b4c853b6597f4cc56c245c0878db6
Jeremy Evans <jeremy.n.evans@gmail.com>
2017-05-30 19:04:07 +0100

    Brought back changes to quizQuestions38Degrees.js

--- public/data/quizQuestions38Degrees.js
+++ public/data/quizQuestions38Degrees.js
@@ -4,11 +4,10 @@ module.exports = {
   }
 };
 
-
-
 var quizQuestions = [
  {
    question: "Should taxes be increased to fund the NHS and social care?",
+   randomiseGroup: "firstThree",
    issue: "tax",
    debate: "38-tax-1",
    helperCard:"http://api.explaain.com/Detail/5929a06cc3fde500115269a3",
@@ -37,6 +36,7 @@ var quizQuestions = [
  },
  {
    question: "Should the pensions 'triple lock' remain in use?",
+   randomiseGroup: "firstThree",
    issue: "welfare",
    debate: "38-welfare-1",
    helperCard:"http://api.explaain.com/Detail/592da145c45b930011674602",
@@ -65,6 +65,7 @@ var quizQuestions = [
  },
  {
    question: "Should the spare room subsidy (so called 'bedroom tax') be scrapped?",
+   randomiseGroup: "firstThree",
    issue: "welfare",
    debate: "38-welfare-2",
    helperCard:"http://api.explaain.com/Detail/5929a172c3fde500115269a5",
@@ -93,6 +94,7 @@ var quizQuestions = [
  },
  {
    question: "Should corporation tax rates be increased?",
+   randomiseGroup: "remainder",
    issue: "tax",
    debate: "38-tax-2",
    helperCard:"http://api.explaain.com/Detail/5929a1b4c3fde500115269a6",
@@ -119,92 +121,96 @@ var quizQuestions = [
      ]
    }
  },
- {
-   question: "Should we get rid of the deficit by 2025?",
-   issue: "economy",
-   debate: "38-economy-1",
-   helperCard:"http://api.explaain.com/Detail/5929a232c3fde500115269a8",
-   answers: {
-     yes: [
-       {
-         label: "aim for a balanced budget by 2025.",
-         opinion: 1,
-       },
-       {
-         label: "but aim to do it within 5 years",
-         opinion: 0.8,
-       },
-     ],
-     no: [
-       {
-         label: "reducing it is important but not at the cost of cutting public services.",
-         opinion: 0.4,
-       },
-       {
-         label: "I think we should eliminate it before 2025.",
-         opinion: 0
-       },
-     ]
-   }
- },
- {
-   question: "Should there be a cap on how much landlords can charge for rent?",
-   issue: "living-costs",
-   debate: "38-living-costs-1",
-   helperCard:"http://api.explaain.com/Detail/5929a25ac3fde500115269a9",
-   answers: {
-     yes: [
-       {
-         label: "there should be a living rent for all through rent controls. ",
-         opinion: 1,
-       },
-       {
-         label: "by capping rent on certain new contracts at the level of inflation. ",
-         opinion: 0.8,
-       },
-     ],
-     no: [
-       {
-         label: "the free market should dictate rent costs.",
-         opinion: 0.4,
-       },
-       {
-         label: "but increasing the number of houses built will make rent more affordable.",
-         opinion: 0
-       },
-     ]
-   }
- },
- {
-   question: "Should those hiring migrant workers have to pay a fee to government to do so?",
-   issue: "immigration",
-   debate: "38-immigration-1",
-   helperCard:"http://api.explaain.com/Detail/5929a296c3fde500115269aa",
-   answers: {
-     yes: [
-       {
-         label: "those who employ a migrant full time should pay £2000 a year.",
-         opinion: 1,
-       },
-       {
-         label: "but make it a voluntary contribution as part of certain types of visa.",
-         opinion: 0.8,
-       },
-     ],
-     no: [
-       {
-         label: "don't increase it, create a 'Migration Impact Fund' paid for centrally by exisitng budgets.",
-         opinion: 0.4,
-       },
-       {
-         label: "there is no need for this type of charge.",
-         opinion: 0
-       },
-     ]
-   }
- },
+ // {
+ //   question: "Should we get rid of the deficit by 2025?",
+ //   randomiseGroup: "remainder",
+ //   issue: "economy",
+ //   debate: "38-economy-1",
+ //   helperCard:"http://api.explaain.com/Detail/5929a232c3fde500115269a8",
+ //   answers: {
+ //     yes: [
+ //       {
+ //         label: "aim for a balanced budget by 2025.",
+ //         opinion: 1,
+ //       },
+ //       {
+ //         label: "but aim to do it within 5 years",
+ //         opinion: 0.8,
+ //       },
+ //     ],
+ //     no: [
+ //       {
+ //         label: "reducing it is important but not at the cost of cutting public services.",
+ //         opinion: 0.4,
+ //       },
+ //       {
+ //         label: "I think we should eliminate it before 2025.",
+ //         opinion: 0
+ //       },
+ //     ]
+ //   }
+ // },
+ // {
+ //   question: "Should there be a cap on how much landlords can charge for rent?",
+ //   randomiseGroup: "remainder",
+ //   issue: "living-costs",
+ //   debate: "38-living-costs-1",
+ //   helperCard:"http://api.explaain.com/Detail/5929a25ac3fde500115269a9",
+ //   answers: {
+ //     yes: [
+ //       {
+ //         label: "there should be a living rent for all through rent controls. ",
+ //         opinion: 1,
+ //       },
+ //       {
+ //         label: "by capping rent on certain new contracts at the level of inflation. ",
+ //         opinion: 0.8,
+ //       },
+ //     ],
+ //     no: [
+ //       {
+ //         label: "the free market should dictate rent costs.",
+ //         opinion: 0.4,
+ //       },
+ //       {
+ //         label: "but increasing the number of houses built will make rent more affordable.",
+ //         opinion: 0
+ //       },
+ //     ]
+ //   }
+ // },
+ // {
+ //   question: "Should those hiring migrant workers have to pay a fee to government to do so?",
+ //   randomiseGroup: "remainder",
+ //   issue: "immigration",
+ //   debate: "38-immigration-1",
+ //   helperCard:"http://api.explaain.com/Detail/5929a296c3fde500115269aa",
+ //   answers: {
+ //     yes: [
+ //       {
+ //         label: "those who employ a migrant full time should pay £2000 a year.",
+ //         opinion: 1,
+ //       },
+ //       {
+ //         label: "but make it a voluntary contribution as part of certain types of visa.",
+ //         opinion: 0.8,
+ //       },
+ //     ],
+ //     no: [
+ //       {
+ //         label: "create a 'Migration Impact Fund' paid for centrally by exisitng budgets.",
+ //         opinion: 0.4,
+ //       },
+ //       {
+ //         label: "there is no need for this type of charge.",
+ //         opinion: 0
+ //       },
+ //     ]
+ //   }
+ // },
  {
    question: "Should there be a fresh vote in parliament to bring back fox hunting?",
+   randomiseGroup: "remainder",
    issue: "environment",
    debate: "38-environment-1",
    helperCard:"http://api.explaain.com/Detail/5929a1dbc3fde500115269a7",
@@ -233,6 +239,7 @@ var quizQuestions = [
  },
  {
    question: "Should the national living wage be increased by more than inflation?",
+   randomiseGroup: "remainder",
    issue: "living-costs",
    debate: "38-living-costs-2",
    helperCard:"http://api.explaain.com/Detail/5929a2bfc3fde500115269ab",
@@ -259,37 +266,39 @@ var quizQuestions = [
      ]
    }
  },
- {
-   question: "Should income tax go up for people earning over £80,000 per year?",
-   issue: "tax",
-   debate: "38-tax-3",
-   helperCard:"http://api.explaain.com/Detail/5929a302c3fde500115269ac",
-   answers: {
-     yes: [
-       {
-         label: "and gaurantee tax won't go up for anyone earning less than £80,000. ",
-         opinion: 1,
-       },
-       {
-         label: "but look at increasing corporation tax at the same time. ",
-         opinion: 1,
-       },
-     ],
-     no: [
-       {
-         label: "increase tax for all taxpayers via an additional 1p extra per £ of income tax.",
-         opinion: 0.4,
-       },
-       {
-         label: "and increase the amount a person can earn before being taxed. ",
-         opinion: 0
-       },
-     ]
-   }
- },
+ // {
+ //   question: "Should income tax go up for people earning over £80,000 per year?",
+ //   randomiseGroup: "remainder",
+ //   issue: "tax",
+ //   debate: "38-tax-3",
+ //   helperCard:"http://api.explaain.com/Detail/5929a302c3fde500115269ac",
+ //   answers: {
+ //     yes: [
+ //       {
+ //         label: "and  gaurantee tax won't go up for anyone earning less than £80,000. ",
+ //         opinion: 1,
+ //       },
+ //       {
+ //         label: "but look at increasing corporation tax at the same time. ",
+ //         opinion: 1,
+ //       },
+ //     ],
+ //     no: [
+ //       {
+ //         label: "increase tax for all taxpayers via an additional 1p extra per £ of income tax.",
+ //         opinion: 0.4,
+ //       },
+ //       {
+ //         label: "and increase the amount a person can earn before being taxed. ",
+ //         opinion: 0
+ //       },
+ //     ]
+ //   }
+ // },
  {
    question: "Should we ban pesticides which kill bees?",
-   issue: "enviornment",
+   randomiseGroup: "remainder",
+   issue: "environment",
    debate: "38-environment-2",
    helperCard:"http://api.explaain.com/Detail/5929a32bc3fde500115269ad",
    answers: {
@@ -345,6 +354,7 @@ var quizQuestions = [
  // },
  {
    question: "Should the government take over the running of railways, water companies and the Royal Mail?",
+   randomiseGroup: "remainder",
    issue: "economy",
    debate: "38-economy-2",
    helperCard:"http://api.explaain.com/Detail/5929a36ec3fde500115269ae",
@@ -373,6 +383,7 @@ var quizQuestions = [
  },
  {
    question: "Should we create additional grammar schools?",
+   randomiseGroup: "remainder",
    issue: "education",
    debate: "38-education-1",
    helperCard:"http://api.explaain.com/Detail/5929a878c3fde500115269af",
@@ -401,6 +412,7 @@ var quizQuestions = [
  },
  {
    question: "Should fracking be banned?",
+   randomiseGroup: "remainder",
    issue: "environment",
    debate: "38-environment-3",
    helperCard:"http://api.explaain.com/Detail/5929a89ec3fde500115269b0",
@@ -411,7 +423,7 @@ var quizQuestions = [
          opinion: 1,
        },
        {
-         label: "because of its impact on climate change and the local enviornment. ",
+         label: "because of its impact on climate change and the local environment. ",
          opinion: 0.8,
        },
      ],
@@ -429,6 +441,7 @@ var quizQuestions = [
  },
  {
    question: "Should we be trying to reduce immigration below 100,000 extra new arrivals per year?",
+   randomiseGroup: "remainder",
    issue: "immigration",
    debate: "38-immigration-2",
    helperCard:"http://api.explaain.com/Detail/5929a8dac3fde500115269b1",
